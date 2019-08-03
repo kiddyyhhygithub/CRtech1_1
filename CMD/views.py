2af6c554640e8ec5fdb7cfe0f969cd7c10d015e3
richie <kiddyflash@163.com>
2019-08-03 14:45:12 +0800

    WF000041801FF

--- CMD/views.py
+++ CMD/views.py
@@ -1,15 +1,17 @@
-from django.shortcuts import render
+﻿from django.shortcuts import render
 from django.conf import settings
 from django.shortcuts import HttpResponse
 # Create your views here.
 import os
 import yh.hy.IoTUtility.IoT as iotut
-import yh.hy.Const.IoTConst as iotconst
+# import yh.hy.Const.IoTConst as iotconst
 from django.core.cache import cache
 import json
 import threading
 
 local = threading.local()
+count = 0
+bad = 0
 
 def cmd(request):
     local.cmdcode = None
@@ -108,94 +110,110 @@ def cmd(request):
     # cmdinf = None
     # cmdcode = local.cmdinf
     # cmdinf = local.cmdcode
+
     # threading.Thread(target=distribute(request=request)).start()
     # # return HttpResponse(cmdcode,cmdinf)
     # return HttpResonse('finish')
 
     # threading.Thread(target=distribute(request)).start()
-    
-    # bodyStr = json.loads(request.body.decode('utf-8'))
-    # threading.Thread(target=distribute,args=(bodyStr)).start()
-    # print(request.body.decode('utf-8'))
-    # print(bodyStr)
-
-    t = threading.Thread(target=distribute,args=(request,))
+    # print(request.body)
+    t = ThreadClass(request)
+    t.setDaemon(True)
     t.start()
-    # t.join(timeout=20)
+    t.join(timeout=6)
     pass
 
     if local.cmdcode == None:
         local.cmdcode = 100
     return HttpResponse(local.cmdcode)
-    # return HttpResponse('yh')
 
 
-# 可用 供调试
-def test2(request):
-    try:
-        # bodyStr = json.loads(request.body.decode('utf-8'))
-        # print(bodyStr)
-        # protocol = bodyStr['services'][0]['data']['serviceData']['MeterReading']['datas'][0]['protocol']
-        # print('\033[31m %s' % (protocol))
-        # print('*********************')
+class ThreadClass(threading.Thread):
+    def __init__(self,request):
+        super().__init__()
+        self._request = request
+    def run(self) -> None:
+        global count
+        count += 1
 
-        print(f'开始分配线程,--线程名:[{threading.currentThread().getName()}],--线程总数:{threading.activeCount()-1}')
+        print('开始分配线程')
+        bodyStr = None
+        protocol = 'ERR00000'
+        try:
+            bodyStr = json.loads(self._request.body.decode('utf-8'))
+            protocol = bodyStr['services'][0]['data']['serviceData']['MeterReading']['datas'][0]['protocol']
+            print(f'******************************{protocol}***************************{threading.current_thread().getName()}***')
 
-        print(request.body.decode('utf-8'))
-        # return None
-        bodyStr = json.loads(request.body.decode('utf-8'))
-        print(str(bodyStr))
+            if protocol != 'NF0000442':
+                # return HttpResponse('ERR0')
+                print('ERRO')
+                return None
 
-        protocol = bodyStr['services'][0]['data']['serviceData']['MeterReading']['datas'][0]['protocol']
-        print('\033[31m %s' % (protocol))
-        print('************************************%s********************************' % (protocol))
-        if protocol != 'NF0000442':
-            # return HttpResponse('ERR0')
-            print('DATA REPORTING')
-            return None
-        ca = (
-            os.path.join(settings.CA_ROOT, r'server.crt'),
-            os.path.join(settings.CA_ROOT, r'server.key'),
-        )
-        token = cache.get('token')
+            ## 这里应该需要上锁
+            ca = (
+                os.path.join(settings.CA_ROOT, r'server.crt'),
+                os.path.join(settings.CA_ROOT, r'server.key'),
+            )
+            token = cache.get('token')
 
-        appid = r'4Ixi2dFrkeCLDchfpQwRsSYS0ewa'
-        secret = r'JvitPmDQuLoncCiQyYzYOsBaIK4a'
-        iot = iotut.IoT(_appid=appid, _secret=secret, _cert=ca)  # 必须要放if外面，否则会报错
-        if token == None:
-            token = iot.getToken()
-            print('reward Token success')
-            cache.set('token', token, 60 * 59)
-            filePath = os.path.join(settings.CA_ROOT, r'token_his.extend')
-            with open(filePath, 'a+') as f:
-                f.writelines(token + '\n')
+            appid = r'4Ixi2dFrkeCLDchfpQwRsSYS0ewa'
+            secret = r'JvitPmDQuLoncCiQyYzYOsBaIK4a'
+            iot = iotut.IoT(_appid=appid, _secret=secret, _cert=ca)  # 必须要放if外面，否则会报错
 
-        print('token', token)
-        deviceId = '4720af4a-4d47-42ed-a421-40cc05d24279'  # 吴老师模块
-        # deviceId = '7261349d-482e-4454-bc0a-452bb2d533ee'#颜华模块
-        serviceId = r'ZmSer'
-        method = r'CMD'
-        cmdValue = r'A00000'
-        # 是否是立即下发
-        EXPIRETIME = 0
-        print('开始下发命令，命令有效时间是-->>  %d   (s)' % (EXPIRETIME))
-        # local.cmdcode = None
-        # local.cmdinf = None
-        cmdcode, cmdinf = iot.CMD(_token=token,
-                                  _deviceId=deviceId,
-                                  _serviceId=serviceId,
-                                  _method=method,
-                                  _cmdvalue=cmdValue,
-                                  _expireTime=EXPIRETIME,
-                                  _callbackUrl=None)
-        local.cmdcode, local.cmdinf = cmdcode, cmdinf
-        print(local.cmdcode, local.cmdinf)
+            if token == None:
+                token = iot.getToken()
+                cache.set('token', token, 60 * 30)
+                filePath = os.path.join(settings.CA_ROOT, r'token_his.extend')
+                # 最好不要写操作
+                # with open(filePath, 'a+') as f:
+                #     f.writelines(token + '\n')
 
-        iot = None
-        print('******************************** c a   ^^^^^^^^^^^^^^^^^^^^^^^^^')
-    except Exception as ex:
-        print(ex)
-    return None
+            print('token', token)
+            deviceId = '4720af4a-4d47-42ed-a421-40cc05d24279'  # 吴老师模块
+            # deviceId = '7261349d-482e-4454-bc0a-452bb2d533ee'  # 颜华模块
+            # deviceId = '7efabe56-063a-4a42-b258-a458993c056b'  # 模拟器模块
+
+            serviceId = r'ZmSer'
+            method = r'CMD'
+            cmdValue = r'WF00004180100'
+            # cmdValue注明：前八位必须是协议号  ，这里 01表示一个字节，FF（表示让单片机进入主动模式），如果是00 就是让南向进入被动模式，非00就默认是主动模式
+
+
+
+            # 是否是立即下发
+            EXPIRETIME = 0
+            print('开始下发命令，命令有效时间是-->>  %d   (s)' % (EXPIRETIME))
+            # local.cmdcode = None
+            # local.cmdinf = None
+            cmdcode, cmdinf = iot.CMD(_token=token,
+                                      _deviceId=deviceId,
+                                      _serviceId=serviceId,
+                                      _method=method,
+                                      _cmdvalue=cmdValue,
+                                      _expireTime=EXPIRETIME,
+                                      _callbackUrl=None)
+            local.cmdcode, local.cmdinf = cmdcode, cmdinf
+            print(local.cmdcode, local.cmdinf)
+
+            iot = None
+        except Exception as ex:
+            print(f'错误信息为：  ---    {ex}')
+
+            global bad
+            bad += 1
+            pass
+
+        # if protocol != 'NF0000442':
+        #     # return HttpResponse('ERR0')
+        #     print('ERRO')
+        #     return None
+        # ca = (
+        #     os.path.join(settings.CA_ROOT, r'server.crt'),
+        #     os.path.join(settings.CA_ROOT, r'server.key'),
+        # )
+        # token = cache.get('token')
+        print(f'end-[{bad}/{count}]----[{bad/count * 100}%]')
+        pass
 
 def test(request):
     # try:
@@ -221,27 +239,18 @@ def test(request):
 #             _expireTime=_expireTime,
 #             _callbackUrl=_callbackUrl)
 #     pass
+
+
 def distribute(request):
     try:
-        # bodyStr = json.loads(request.body.decode('utf-8'))
-        # print(bodyStr)
-        # protocol = bodyStr['services'][0]['data']['serviceData']['MeterReading']['datas'][0]['protocol']
-        # print('\033[31m %s' % (protocol))
-        # print('*********************')
-
-        print(f'开始分配线程,--线程名:[{threading.currentThread().getName()}],--线程总数:{threading.activeCount()-1}')
-
-        # print(request.body.decode('utf-8'))
-        # return None
+        print('开始分配线程')
         bodyStr = json.loads(request.body.decode('utf-8'))
-        # print(str(bodyStr))
-
         protocol = bodyStr['services'][0]['data']['serviceData']['MeterReading']['datas'][0]['protocol']
-        # print('\033[31m %s' % (protocol))
-        print('************************************%s********************************' % (protocol))
+        print(protocol)
+        # return HttpResponse(protocol)
         if protocol != 'NF0000442':
             # return HttpResponse('ERR0')
-            print('DATA REPORTING')
+            print('ERRO')
             return None
         ca = (
             os.path.join(settings.CA_ROOT, r'server.crt'),
@@ -252,10 +261,10 @@ def distribute(request):
         appid = r'4Ixi2dFrkeCLDchfpQwRsSYS0ewa'
         secret = r'JvitPmDQuLoncCiQyYzYOsBaIK4a'
         iot = iotut.IoT(_appid=appid, _secret=secret, _cert=ca)  # 必须要放if外面，否则会报错
+
         if token == None:
             token = iot.getToken()
-            print('reward Token success')
-            cache.set('token', token, 60 * 59)
+            cache.set('token', token, 60 * 30)
             filePath = os.path.join(settings.CA_ROOT, r'token_his.extend')
             with open(filePath, 'a+') as f:
                 f.writelines(token + '\n')
@@ -263,70 +272,6 @@ def distribute(request):
         print('token', token)
         deviceId = '4720af4a-4d47-42ed-a421-40cc05d24279'  # 吴老师模块
         # deviceId = '7261349d-482e-4454-bc0a-452bb2d533ee'#颜华模块
-        serviceId = r'ZmSer'
-        method = r'CMD'
-        cmdValue = r'A00000'
-        # 是否是立即下发
-        EXPIRETIME = 0
-        print('开始下发命令，命令有效时间是-->>  %d   (s)' % (EXPIRETIME))
-        # local.cmdcode = None
-        # local.cmdinf = None
-        cmdcode, cmdinf = iot.CMD(_token=token,
-                                  _deviceId=deviceId,
-                                  _serviceId=serviceId,
-                                  _method=method,
-                                  _cmdvalue=cmdValue,
-                                  _expireTime=EXPIRETIME,
-                                  _callbackUrl=None)
-        local.cmdcode, local.cmdinf = cmdcode, cmdinf
-        print(local.cmdcode, local.cmdinf)
-
-        iot = None
-        print('*---------------------------- e n d -----------------------------*')
-    except Exception as ex:
-        print(ex)
-    return None
-
-
-# 可用  作废
-def distribute1(request):
-    try:
-        print('开始分配线程')
-
-        print(request.body.decode('utf-8'))
-        return None
-        bodyStr = json.loads(request.body.decode('utf-8'))
-        print(str(bodyStr))
-
-        protocol = bodyStr['services'][0]['data']['serviceData']['MeterReading']['datas'][0]['protocol']
-        print('\033[31m %s'%(protocol))
-        print('************************************%s********************************'%(protocol))
-        # return HttpResponse(protocol)
-        if protocol != 'NF0000442':
-            # return HttpResponse('ERR0')
-            print('DATA REPORTING')
-            return None
-        ca = (
-            os.path.join(settings.CA_ROOT, r'server.crt'),
-            os.path.join(settings.CA_ROOT, r'server.key'),
-        )
-        token = cache.get('token')
-
-        appid = r'4Ixi2dFrkeCLDchfpQwRsSYS0ewa'
-        secret = r'JvitPmDQuLoncCiQyYzYOsBaIK4a'
-        iot = iotut.IoT(_appid=appid, _secret=secret, _cert=ca)  # 必须要放if外面，否则会报错
-
-        if token == None:
-            token = iot.getToken()
-            print('reward Token success')
-            cache.set('token', token, 60 * 59)
-            filePath = os.path.join(settings.CA_ROOT, r'token_his.extend')
-            with open(filePath, 'a+') as f:
-                f.writelines(token + '\n')
-
-        print('token', token)
-        deviceId = '4720af4a-4d47-42ed-a421-40cc05d24279'  # 吴老师模块
-         # deviceId = '7261349d-482e-4454-bc0a-452bb2d533ee'#颜华模块
         serviceId = r'ZmSer'
         method = r'CMD'
         cmdValue = r'A00000'
@@ -348,6 +293,6 @@ def distribute1(request):
 
         iot = None
     except Exception as ex:
-        print('分配线程后下发命令异常: ',ex)
+        print(ex)
     # local.cmdcode, local.cmdinf = cmdcode, cmdinf
 
